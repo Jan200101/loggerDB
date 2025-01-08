02cccad339770557ff0378458789883427cd492f
Jan200101 <sentrycraft123@gmail.com>
2025-01-08 12:30:32 +0100

    change win32 check, lowercase header for mingw

--- loggerDB/include/loggerDB/compat.h
+++ loggerDB/include/loggerDB/compat.h
@@ -1,11 +1,11 @@
 #ifndef LOGGERDB_COMPAT_H
 #define LOGGERDB_COMPAT_H
 
-#if defined(_MSC_VER)
+#if (defined(_WIN32) || defined(__WIN32__))
 
 #define mkdir(A, B) mkdir(A)
 
-#include <BaseTsd.h>
+#include <basetsd.h>
 typedef SSIZE_T ssize_t;
 
 #endif
